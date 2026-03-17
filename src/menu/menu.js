cd1c08d7304fa2e4dbb29573588d312833991df6
Gody <250132528+matecg@users.noreply.github.com>
2026-03-17 07:27:43 -0300

    Add Menu section with basic styling

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -3,11 +3,17 @@ import MenuData from "./menu.json";
 
 export function Menu() {
     const parent = document.createElement("div");
+    parent.classList.add("menu");
 
     const h2 = document.createElement("h2");
     h2.classList.add("menu-title");
     h2.textContent = "Our Menu";
     parent.appendChild(h2);
+
+    const pricePara = document.createElement("p");
+    pricePara.textContent = "All our prices are charged in gold coins 🪙! (We do not accept silver or copper ones, but Bitcoin might be good as well)"
+    pricePara.classList.add("price-para");
+    parent.appendChild(pricePara);
 
     Object.keys(MenuData.menu).forEach(key => {
         const section = createMenuSection(key, MenuData.menu[key]);
@@ -19,23 +25,28 @@ export function Menu() {
 
 function createMenuSection(name, items) {
     const parent = document.createElement("div");
+    parent.classList.add("menu-section");
 
     const h3 = document.createElement("h3");
     h3.textContent = name.replaceAll("_", " ");
 
     const itemsContainer = document.createElement("div");
+    itemsContainer.classList.add("menu-container");
     items.forEach(item => {
+        const itemCard = document.createElement("div");
         const itemTitle = document.createElement("p");
         const itemPrice = document.createElement("p");
         const itemDescription = document.createElement("p");
 
+        itemCard.classList.add("menu-card");
         itemTitle.textContent = item.title;
         itemPrice.textContent = `🪙 ${item.price} g.c.`;
         itemDescription.textContent = item.description;
 
-        itemsContainer.appendChild(itemTitle);
-        itemsContainer.appendChild(itemPrice);
-        itemsContainer.appendChild(itemDescription);
+        itemCard.appendChild(itemTitle);
+        itemCard.appendChild(itemPrice);
+        itemCard.appendChild(itemDescription);
+        itemsContainer.appendChild(itemCard);
     });
 
     parent.appendChild(h3);
