7003ff0002e44a906c33d9fa7592506b8122207d
Gody <250132528+matecg@users.noreply.github.com>
2026-03-17 10:27:21 -0300

    Add about section without much styling

--- src/about/about.js
+++ src/about/about.js
@@ -1,9 +1,47 @@
+import AboutData from "./about.json";
+import "./about.css";
+import MapImg from "./map.png"
+
 export function About() {
     const parent = document.createElement("div");
 
-    const para = document.createElement("p");
-    para.textContent = "I'm the About component!";
+    const h2 = document.createElement("h2");
+    h2.textContent = "About Us";
+    parent.appendChild(h2);
 
-    parent.appendChild(para);
+    const sectionContainer = document.createElement("div");
+    sectionContainer.classList.add('about-container');
+    AboutData.about_page.about_sections.forEach(({title, content}) => {
+        const img = title === "Finding the Feast" ? MapImg : null;
+        const aboutSection = createAboutSection(title, content, img);
+        sectionContainer.appendChild(aboutSection);
+    });
+    parent.appendChild(sectionContainer);
+
     return parent;
+}
+
+function createAboutSection(title, content, img = null) {
+    const container = document.createElement("div");
+    const h3 = document.createElement("h3");
+    const contentPara = document.createElement("p");
+
+    h3.textContent = title;
+    contentPara.textContent = content;
+    container.appendChild(h3);
+
+    if (img) {
+        const imgContainer = document.createElement("div");
+        imgContainer.classList.add("about-img");
+        const imgTag = document.createElement("img");
+        imgTag.src = img;
+        imgContainer.appendChild(imgTag);
+        imgContainer.appendChild(contentPara);
+        container.appendChild(imgContainer);
+        return container;
+    }
+    
+    container.appendChild(contentPara);
+
+    return container;
 }
